99a7981f6d06333bb10daabd575f209f645108bf
Franco-04 <davidfranco7329@gmail.com>
2025-10-07 21:47:08 -0600

    c

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,8 @@
-const index = require ()
+const hash = require('./testindex');
+
+test('Verificar hash de UTEQ', () => {
+    const message = "UTEQ";
+    const hashtest = hash(message);
+
+    expect(hashtest).toEqual("892f78a98921edc19e4c8ddd9208b5ca90c9d02eddd1d050583035e16f0ffbd371b2959d30627763d61d01fd473a98dde1c0856ddd8eceb960d74dcdd44c58f1");
+});
